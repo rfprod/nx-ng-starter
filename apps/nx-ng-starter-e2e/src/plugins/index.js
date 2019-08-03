9b21855d8128a1e0b7ed985fc630824aee809b12
rfprod <zoidenmacht@zoho.com>
2019-08-04 02:48:10 +0300

    chore(project-dependencies): updated projct dependencies
    updated project dependencies

--- apps/nx-ng-starter-e2e/src/plugins/index.js
+++ apps/nx-ng-starter-e2e/src/plugins/index.js
@@ -1,4 +1,4 @@
-// ***********************************************************
+const { preprocessTypescript } = require('@nrwl/cypress/plugins/preprocessor'); // ***********************************************************
 // This example plugins/index.js can be used to load plugins
 //
 // You can change the location of this file or turn off loading
@@ -7,11 +7,10 @@
 // You can read more here:
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
-
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
-
-module.exports = (on: Cypress.Actions, config: Cypress.ConfigOptions) => {
+module.exports = function(on, config) {
+  on('file:preprocessor', preprocessTypescript(config));
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 };
