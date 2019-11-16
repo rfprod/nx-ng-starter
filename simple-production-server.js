0a35e7c64250922a79adc54242d81c601741f0d9
rfprod <zoidenmacht@zoho.com>
2019-11-16 20:45:31 +0300

    feat(simple-production-server): added simple production server, SW doesn't work with ng serve
    added simple production server, SW doesn't work with ng serve

---
+++ simple-production-server.js
@@ -0,0 +1,115 @@
+'use strict';
+
+/**
+ * Server module
+ * @module server
+ */
+
+/**
+ * Set process title.
+ */
+process.title = 'simple-production-server';
+
+/**
+ * @name cwd
+ * @constant
+ * @summary Current directory of the main Server script - server.js
+ * @description Correct root path for all setups, it should be used for all file references for the server and its modules like filePath: cwd + '/actual/file.extension'. Built Electron app contains actual app in resources/app(.asar) subdirectory, so it is essential to prefer __dirname usage over process.cwd() to get the value.
+ */
+const cwd = __dirname;
+
+/**
+ * @name express
+ * @constant
+ * @summary Express server
+ * @description Express server
+ */
+const express = require('express');
+/**
+ * @name compression
+ * @constant
+ * @summary Compression for Express server
+ * @description Compression for Express server
+ */
+const compression = require('compression');
+
+/**
+ * @name app
+ * @constant
+ * @summary Express application
+ * @description Express application
+ */
+const app = express();
+
+/**
+ * Use compression for all responses.
+ */
+app.use(
+  compression({
+    threshold: 0,
+    level: -1,
+  }),
+);
+
+/**
+ * Serve public directory.
+ */
+app.use('/', express.static(cwd + '/dist/apps/nx-ng-starter'));
+
+/**
+ * Serve app index file for paths excluding provided in regX.
+ */
+app.use((req, res, next) => {
+  /*
+   *	this is required for angular to load urls properly when user requests url directly, e.g.
+   *	current conditions: client index page is served fro all request
+   *	which do not include control words: api, css, fonts, img, js
+   *	control words explanation:
+   *	api - is part of path that returnd data over REST API
+   * css, fonts, img, js - are directories containing client files
+   */
+  // console.log('req.path:', req.path);
+  const regX = /(css|webfonts|img|js|woff|woff2|ttf)/;
+
+  console.log('req', req.path);
+
+  if (regX.test(req.path)) {
+    return next();
+  } else {
+    res.sendFile(cwd + '/dist/apps/nx-ng-starter/index.html');
+  }
+});
+
+/**
+ * Headers config for all Express api routes.
+ */
+app.all('/*', function(req, res, next) {
+  /** CORS headers */
+  res.header('Access-Control-Allow-Origin', '*'); // restrict it to the required domain if needed
+  res.header('Allow', 'GET,PUT,POST,DELETE,OPTIONS,HEAD');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,HEAD');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-type,Content-Length,Accept,X-Access-Token,X-Key,X-Auth-Token,X-Requested-With,Authorization',
+  );
+  /** additions headers to be exposed */
+  res.header('Access-Control-Expose-Headers', 'Views,X-Auth-Token');
+  /** cache control */
+  res.header('Cache-Control', 'public, no-cache, no-store, must-ravalidate, max-age=0');
+  res.header('Expires', '-1');
+  res.header('Pragma', 'no-cache');
+  /** handle OPTIONS method */
+  if (req.method == 'OPTIONS') res.status(200).end();
+  else next();
+});
+
+/**
+ * @name port
+ * @summary Application port
+ * @description Application port
+ */
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`\n# > SIMPLE PRODUCTION SERVER STARTED > Node.js listening on ${port}...\n`);
+});
