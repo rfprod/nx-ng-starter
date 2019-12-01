56afd73e0dc0f301263fab05c500154bc807c635
rfprod <zoidenmacht@zoho.com>
2019-12-01 18:14:46 +0300

    test(jest-config): fixed jest config - added globals, and collect coverage by default
    fixed jest config - added globals, and collect coverage by default

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,22 @@
 module.exports = {
   testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.html$',
+      astTransformers: [
+        'jest-preset-angular/InlineFilesTransformer',
+        'jest-preset-angular/StripStylesTransformer',
+        'jest-preset-angular/InlineHtmlStripStylesTransformer',
+      ],
+    },
+  },
   transform: {
     '^.+\\.(ts|js|html)$': 'ts-jest',
   },
+  transformIgnorePatterns: ['node_modules/(?!@ngxs)'],
+  moduleFileExtensions: ['ts', 'html', 'js', 'json'],
   resolver: '@nrwl/jest/plugins/resolver',
-  moduleFileExtensions: ['ts', 'js', 'html'],
   coverageReporters: ['html'],
+  collectCoverage: true,
 };
