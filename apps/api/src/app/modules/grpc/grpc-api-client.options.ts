16a8ba554a238d0bb3319a966a922013bdc7728e
rfprod <zraw.stdio@gmail.com>
2020-04-12 02:18:49 +0300

    fix(function-deployment): fixed functions deployment
    fixed graphql-api-client options, azure pileline, functions predeploy hook

--- apps/api/src/app/modules/grpc/grpc-api-client.options.ts
+++ apps/api/src/app/modules/grpc/grpc-api-client.options.ts
@@ -1,7 +1,14 @@
 import { ClientOptions, Transport } from '@nestjs/microservices';
+import fs from 'fs';
 import { join } from 'path';
 
 export const NXNGSTARTER_PACKAGE = 'NXNGSTARTER_PACKAGE';
+
+const localProtoPath = join(__dirname, '../../../tools/proto/root.proto');
+const functionsProtoPath = join(__dirname, './proto/root.proto');
+
+const protoPaths: () => string[] = () =>
+  fs.existsSync(localProtoPath) ? [localProtoPath] : [functionsProtoPath];
 
 /**
  * Grpc client options.
@@ -10,6 +17,6 @@ export const grpcApiClientOptions: ClientOptions = {
   transport: Transport.GRPC,
   options: {
     package: ['nxngstarter'],
-    protoPath: [join(__dirname, '../../../tools/proto/root.proto')],
+    protoPath: protoPaths(),
   },
 };
