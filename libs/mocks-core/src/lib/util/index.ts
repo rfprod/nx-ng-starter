5fb24b2b384106ffe4e900efce355ec1f0cd5548
rfprod <zraw.stdio@gmail.com>
2020-05-03 12:00:25 +0300

    fix(mocks): return local storage mock export
    added back local storage mock export so that it can be referred as a type

--- libs/mocks-core/src/lib/util/index.ts
+++ libs/mocks-core/src/lib/util/index.ts
@@ -4,4 +4,5 @@
 export * from './jest/jest-jsdom-globals.patch';
 export * from './jest/jest-spies.patch';
 export * from './refs/index';
+export * from './local-storage/local-storage.mock';
 export * from './test-bed/test-bed-config.mock';
