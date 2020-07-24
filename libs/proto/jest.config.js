5243fb9a1bdb9a9c59fe40a76acf686edacf3451
rfprod <zraw.stdio@gmail.com>
2020-07-24 19:32:15 +0300

    chore(dependencies): update project dependencies
    first remove tsconfig base for upgrade to work
    
    chore(nx workspace): update nx workspace
    
    chore(dependencies): updated all project dependencies to latest versions
    
    BREAKING CHANGE
    
    reinstall project dependencies

--- libs/proto/jest.config.js
+++ libs/proto/jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   name: 'proto',
   preset: '../../jest.config.js',
   coverageDirectory: '../../coverage/libs/proto',
+  globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
 };
