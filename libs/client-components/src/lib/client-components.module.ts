f243800ab278da444a5ee98ee6a983817ddcb0fc
rfprod <zraw.stdio@gmail.com>
2020-08-26 19:53:22 +0300

    refactor(client): refactor client components
    added info presentational component
    
    ci(github): improve github actions
    
    improved github actions configuration (more efficient caching);

--- libs/client-components/src/lib/client-components.module.ts
+++ libs/client-components/src/lib/client-components.module.ts
@@ -6,9 +6,15 @@ import { AppClientComponentsRoutingModule } from './client-components-routing.mo
 import { AppHomeComponent } from './home/home.component';
 import { AppIndexComponent } from './index/index.component';
 import { AppInfoComponent } from './info/info.component';
+import { AppInfoPresentationalComponent } from './info/presentational/info-presentational.component';
 
 @NgModule({
   imports: [CommonModule, AppClientMaterialModule, AppClientComponentsRoutingModule],
-  declarations: [AppIndexComponent, AppHomeComponent, AppInfoComponent],
+  declarations: [
+    AppIndexComponent,
+    AppHomeComponent,
+    AppInfoComponent,
+    AppInfoPresentationalComponent,
+  ],
 })
 export class AppClientComponentsModule {}
