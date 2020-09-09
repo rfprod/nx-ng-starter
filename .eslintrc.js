fd707314febd8b0f81aea26bcfff4e8d722e450d
rfprod <zraw.stdio@gmail.com>
2020-09-09 14:27:02 +0300

    chore(dependencies): update project dependencies
    updated dependencies, verified project workability after;
    
    BREAKING CHANGE: reinstall project dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier',
+    'plugin:prettier/recommended',
     'prettier/@typescript-eslint',
   ],
   plugins: [
