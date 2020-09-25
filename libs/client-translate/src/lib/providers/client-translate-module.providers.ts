87c9dcfd38582a64968d58b582d6d127129c1ed5
rfprod <zraw.stdio@gmail.com>
2020-09-25 12:42:14 +0300

    chore(dependencies): update project dependencies
    updated project dependencies;
    verified project workability after update;
    
    BREAKING CHANGE: reinstall project dependencies

--- libs/client-translate/src/lib/providers/client-translate-module.providers.ts
+++ libs/client-translate/src/lib/providers/client-translate-module.providers.ts
@@ -1,6 +1,7 @@
 import { Provider } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
+import { EN, EN_DICTIONARY } from '../dictionaries/en';
 import { RU, RU_DICTIONARY } from '../dictionaries/ru';
 
 /**
@@ -9,4 +10,5 @@ import { RU, RU_DICTIONARY } from '../dictionaries/ru';
 export const appSharedUiTranslateModuleProviders: Provider[] = [
   TranslateService,
   { provide: RU_DICTIONARY, useValue: RU },
+  { provide: EN_DICTIONARY, useValue: EN },
 ];
