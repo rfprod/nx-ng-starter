a0befa80c76cf184be47b316f2e88d06a376f041
rfprod <zraw.stdio@gmail.com>
2020-11-02 21:56:47 +0300

    chore(deps): update dependencies, fix commmands
    updated project dependencies;
    fixed pipelines;
    fixed documentation generation;
    
    BREAKING CHANGE: reinstall project dependencies

--- apps/documentation/src/environments/environment.prod.ts
+++ apps/documentation/src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment: IDocAppEnvironment = {
   production: true,
   appName: 'Documentation',
   // eslint-disable-next-line prettier/prettier, @typescript-eslint/comma-spacing
-  mdFilePaths: ['libs/client-services/README.md','libs/client-translate/README.md','tools/ts/README.md','tools/shell/README.md','libs/client-components/README.md','libs/client-core/README.md','libs/client-gql/README.md','libs/client-util/README.md','libs/client-material/README.md','tools/ts/UNIT_COVERAGE.md','libs/proto/README.md','libs/mocks-core/README.md','libs/api-interface/README.md','apps/README.md','libs/client-store/README.md','libs/README.md','tools/README.md'],
+  mdFilePaths: ['libs/client-services/README.md','libs/client-translate/README.md','tools/ts/README.md','tools/shell/README.md','libs/client-components/README.md','libs/client-core/README.md','libs/client-gql/README.md','libs/client-util/README.md','libs/client-material/README.md','tools/ts/UNIT_COVERAGE.md','libs/proto/README.md','libs/mocks-core/README.md','libs/api-interface/README.md','apps/README.md','libs/client-store/README.md','libs/README.md'],
 };
