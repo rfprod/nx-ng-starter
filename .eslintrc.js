7a373d42ba20b89d416fb54b821ebc7e3f9f6161
Vadim <rfprod@users.noreply.github.com>
2020-12-05 15:17:37 +0300

    chore(deps): update project dependencies (#186)
    updated project dependencies;
    verified project workability after update;
    
    BREAKING CHANGE: reinstall project dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig-workspace-js.eslint.json'],
+    project: ['./tsconfig.json', './tsconfig.eslint-js.json'],
   },
   extends: [
     'eslint:recommended',
