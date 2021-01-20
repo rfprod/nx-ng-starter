3242f581e577f665d24f5019f9582152c8b9fecb
rfprod <zraw.stdio@gmail.com>
2021-01-20 17:15:59 +0300

    feat(workspace): add package integrity checker, use in pipelines
    - [X] add package integrity checker based on yarn;
    - [X] use yarn extension in gh actions;

--- tools/shell/README.md
+++ tools/shell/README.md
@@ -16,6 +16,7 @@ This folder contains utility shell scripts.
 - `module-aliases.sh` - supported module aliases script (used by other scripts to determine if module alias exists before performing action)
 - `set-documentation-env.sh` - documentation app prebuild script, collects references to \*.md files across the repo
 - `test.sh` - apps/libs unit testing script
+- `yarn-extension.sh` - checks package integrity and cleans up workspace if there is no integrity
 
 ## `build.sh`
 
---
+++ tools/shell/yarn-extension.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+##
+# Colors.
+##
+source tools/shell/colors.sh ''
+##
+# Printing utility functions.
+##
+source tools/shell/print-utils.sh ''
+
+##
+# Reports usage.
+##
+reportUsage() {
+  printInfoTitle "<< ${0} USAGE >>"
+  printUsageTip "tools/shell/yarn-extension.sh ?" "print script usage"
+  printUsageTip "tools/shell/yarn-extension.sh integrity-check" "performs an integrity check, and cleans up workspace if the is no integrity."
+  printGap
+}
+
+##
+# Checks package integrity, and cleans up workspace and caches if there is no integrity.
+# No integrity mean that installed node_modules do not correspond versions defined in the package.json.
+# This utility is intended to be used by the CI mainly.
+##
+checkIntegrity() {
+  printInfoTitle "<< Checking package integrity >>"
+  printGap
+
+  if ! yarn install --ignore-scripts --frozen-lockfile --check-files --integrity; then
+    printInfoTitle "<< Cleaning up workspace, and caches >>"
+    printGap
+
+    yarn workspace:cleanup
+    rm -rf /tmp/nx/nx-ng-starter /tmp/jest_rs/nx-ng-starter ./node_modules
+    npm cache clean --force
+    yarn cache clean
+  else
+    printSuccessTitle "<< Package integrity verified >>"
+    printGap
+  fi
+}
+
+if [ "$1" = "?" ]; then
+  reportUsage
+elif [ "$1" = "integrity-check" ]; then
+  checkIntegrity
+fi
