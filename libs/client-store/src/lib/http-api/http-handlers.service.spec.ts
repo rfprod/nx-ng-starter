f70a78c534fef9eb4708e41d2157ddca0de73b6a
Vadim <rfprod@users.noreply.github.com>
2021-02-10 15:23:17 +0300

    refactor(services): refactor libraries (#212)
    - [X] move toaster service to client-store

--- libs/client-store/src/lib/http-api/http-handlers.service.spec.ts
+++ libs/client-store/src/lib/http-api/http-handlers.service.spec.ts
@@ -2,11 +2,7 @@ import { HttpErrorResponse, HttpHeaders, HttpRequest } from '@angular/common/htt
 import { HttpTestingController, TestRequest } from '@angular/common/http/testing';
 import { TestBed, TestModuleMetadata, waitForAsync } from '@angular/core/testing';
 import { Store } from '@ngxs/store';
-import {
-  AppToasterService,
-  markdownServiceProvider,
-  toasterServiceProvider,
-} from '@nx-ng-starter/client-services';
+import { markdownServiceProvider } from '@nx-ng-starter/client-services';
 import { AppClientTranslateModule } from '@nx-ng-starter/client-translate';
 import { HTTP_STATUS } from '@nx-ng-starter/client-util';
 import {
@@ -21,6 +17,10 @@ import { Observable, of } from 'rxjs';
 import { concatMap, map, tap } from 'rxjs/operators';
 
 import { AppHttpProgressModule } from '../http-progress/http-progress.module';
+import {
+  AppToasterService,
+  toasterServiceProvider,
+} from '../http-progress/services/toaster/toaster.service';
 import { AppHttpHandlersService } from './http-handlers.service';
 
 describe('AppHttpHandlersService', () => {
