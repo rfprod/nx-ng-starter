44f590d760cb0af5222c50975a97d2ac1ebecb28
rfprod <zraw.stdio@gmail.com>
2021-02-25 15:27:17 +0300

    refactor(workspace): improve/fix workspace generator

--- tools/generators/client-feature/files/jest.config.js
+++ tools/generators/client-feature/files/jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   preset: '../../jest.preset.js',
-  coverageDirectory: '../../coverage/libs/<%= libraryName %>',
+  coverageDirectory: '../../coverage/libs/<%= name %>',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
     'jest-preset-angular/build/HTMLCommentSerializer.js',
   ],
-  displayName: '<%= libraryName %>',
+  displayName: '<%= name %>',
 };
--- tools/generators/client-feature/index.ts
+++ tools/generators/client-feature/index.ts
@@ -1,27 +1,71 @@
+import { strings } from '@angular-devkit/core';
 import {
   apply,
   chain,
   externalSchematic,
+  MergeStrategy,
   mergeWith,
   move,
   Rule,
   SchematicContext,
+  template,
   Tree,
   url,
 } from '@angular-devkit/schematics';
+import { updateProjectConfiguration, Tree as NxTree, ProjectConfiguration, NxJsonProjectConfiguration } from '@nrwl/devkit';
 import { getProjectConfig } from '@nrwl/workspace';
 import * as path from 'path';
 
 import { ISchematicContext } from './schema.interface';
 
+interface IProjectConfig {
+  root: string;
+  sourceRoot: string;
+  projectType: string;
+  prefix: string;
+  architect: {
+    lint: {
+      builder: string;
+      options: {
+        eslintConfig: string;
+        lintFilePatterns: string[];
+      } & Record<string, string>;
+    };
+    test: {
+      builder: string;
+      outputs: string[];
+      options: {
+        jestConfig: string;
+        passWithNoTests: boolean;
+      };
+    };
+  };
+}
+
 const addFiles = (schema: ISchematicContext): Rule => (tree: Tree, context: SchematicContext) => {
-  const projectConfig: { root: string } = getProjectConfig(tree, schema.name);
-  console.log('projectConfig', projectConfig);
+  const projectConfig: IProjectConfig = getProjectConfig(tree, schema.name);
 
-  const templateSource = apply(url('./files'), [move(projectConfig.root)]);
+  const templateSource = apply(url('./files'), [
+    move(projectConfig.root),
+    template({
+      ...strings,
+      ...schema, // pass the objects containing the properties & functions to be used in template file
+    }),
+  ]);
 
-  return chain([mergeWith(templateSource)])(tree, context);
+  return chain([mergeWith(templateSource, MergeStrategy.Overwrite)])(tree, context);
 };
+
+const updateProjectConfig = (schema: ISchematicContext): Rule => (tree: Tree, context: SchematicContext) => {
+  const projectConfig: IProjectConfig & ProjectConfiguration & NxJsonProjectConfiguration = getProjectConfig(tree, schema.name);
+  projectConfig.architect.lint.builder = '@angular-eslint/builder:lint';
+  projectConfig.architect.lint.options.lintFilePatterns = [`libs/${schema.name}/src/**/*.ts`];
+  console.log('projectConfig', JSON.stringify(projectConfig));
+
+  updateProjectConfiguration(tree as unknown as NxTree, schema.name, projectConfig);
+
+  return chain([]);
+}
 
 export default function (schema: ISchematicContext) {
   return (tree: Tree, context: SchematicContext) => {
@@ -29,14 +73,13 @@ export default function (schema: ISchematicContext) {
     const name = schema.name;
     const tags = schema.tags;
 
-    console.log('name', name);
-
     return chain([
       externalSchematic('@nrwl/angular', 'lib', {
         name,
         tags,
         style: 'scss',
       }),
+      updateProjectConfig(schema),
       addFiles(schema),
       externalSchematic('@schematics/angular', 'component', {
         project: name,
