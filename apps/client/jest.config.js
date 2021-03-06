b572f971fd08d9e313e11848fb4ede379dffe2c9
rfprod <zraw.stdio@gmail.com>
2021-03-06 10:54:07 +0300

    chore(deps): bump dependencies, update configs
    - [X] update project dependencies;
    - [X] update jest configs;
    
    BREAKING CHANGE: reinstall project dependencies

--- apps/client/jest.config.js
+++ apps/client/jest.config.js
@@ -3,9 +3,9 @@ module.exports = {
   coverageDirectory: '../../coverage/apps/client',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   snapshotSerializers: [
-    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
-    'jest-preset-angular/build/AngularSnapshotSerializer.js',
-    'jest-preset-angular/build/HTMLCommentSerializer.js',
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
   ],
   displayName: 'client',
 };
