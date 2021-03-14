2afd9436c5373ef0955376a2636aa108835aace8
rfprod <zraw.stdio@gmail.com>
2021-03-14 15:06:21 +0300

    style(lint): fix linting errors

--- libs/client-diagnostics/src/lib/home/page/home-page.component.stories.ts
+++ libs/client-diagnostics/src/lib/home/page/home-page.component.stories.ts
@@ -2,7 +2,12 @@ import { DOCUMENT, LocationStrategy, PathLocationStrategy } from '@angular/commo
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppClientMaterialModule } from '@nx-ng-starter/client-material';
-import { documentFactory, WEB_CLIENT_APP_ENV, WINDOW, windowFactory } from '@nx-ng-starter/client-util';
+import {
+  documentFactory,
+  WEB_CLIENT_APP_ENV,
+  WINDOW,
+  windowFactory,
+} from '@nx-ng-starter/client-util';
 import { text } from '@storybook/addon-knobs';
 
 import { AppHomePage } from './home-page.component';
