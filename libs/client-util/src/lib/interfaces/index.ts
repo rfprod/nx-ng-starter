ba04aed22d62fe586c48c6abdf04ad7037a7c20c
rfprod <zraw.stdio@gmail.com>
2021-03-15 20:50:36 +0300

    refactor(client): make base toolbar client components configurable
    - [X] make base toolbar client components configurable;

--- libs/client-util/src/lib/interfaces/index.ts
+++ libs/client-util/src/lib/interfaces/index.ts
@@ -1,3 +1,4 @@
 export * from './environment.interface';
 export * from './sentry.interface';
 export * from './toaster.interface';
+export * from './toolbar.interface';
---
+++ libs/client-util/src/lib/interfaces/toolbar.interface.ts
@@ -0,0 +1,11 @@
+export interface IToolbarButton {
+  routerLink: string[];
+  icon: string;
+  title: string;
+}
+
+export interface IToolbarAnchor {
+  href: string;
+  icon: string;
+  title: string;
+}
