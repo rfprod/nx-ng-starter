4378bda00c14063db6c7c148afa38a2193ae05f5
Vadim <rfprod@users.noreply.github.com>
2021-03-15 21:18:47 +0300

    refactor(client): make base toolbar client components configurable (#233)
    - [X] make base toolbar client components configurable;

--- libs/client-components/src/lib/navbar/navbar.component.ts
+++ libs/client-components/src/lib/navbar/navbar.component.ts
@@ -1,5 +1,9 @@
-import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
-import { IWebClientAppEnvironment, WEB_CLIENT_APP_ENV } from '@nx-ng-starter/client-util';
+import { ChangeDetectionStrategy, Component, Inject, Input } from '@angular/core';
+import {
+  IToolbarAnchor,
+  IWebClientAppEnvironment,
+  WEB_CLIENT_APP_ENV,
+} from '@nx-ng-starter/client-util';
 
 @Component({
   selector: 'app-navbar',
@@ -8,6 +12,26 @@ import { IWebClientAppEnvironment, WEB_CLIENT_APP_ENV } from '@nx-ng-starter/cli
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppNavbarComponent {
+  @Input() public logoSrc = 'assets/icons/icon-72x72.png';
+
+  @Input() public anchors: IToolbarAnchor[] = [
+    {
+      href: 'https://cli.angular.io/reference.pdf',
+      icon: 'build',
+      title: 'CLI Reference',
+    },
+    {
+      href: 'https://material.angular.io/',
+      icon: 'change_history',
+      title: 'Angular Material',
+    },
+    {
+      href: 'https://material.io/icons/',
+      icon: 'info_outline',
+      title: 'Material Icons',
+    },
+  ];
+
   public readonly appName = this.env.appName;
 
   constructor(@Inject(WEB_CLIENT_APP_ENV) private readonly env: IWebClientAppEnvironment) {}
--- libs/client-components/src/lib/toolbar/toolbar.component.ts
+++ libs/client-components/src/lib/toolbar/toolbar.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Store } from '@ngxs/store';
 import { AppSidebarState, chatbotActions, sidebarUiActions } from '@nx-ng-starter/client-store';
+import { IToolbarButton } from '@nx-ng-starter/client-util';
 import { map } from 'rxjs/operators';
 
 @Component({
@@ -10,6 +11,19 @@ import { map } from 'rxjs/operators';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppToolbarComponent {
+  @Input() public buttons: IToolbarButton[] = [
+    {
+      routerLink: ['info'],
+      icon: 'touch_app',
+      title: 'API info',
+    },
+    {
+      routerLink: [''],
+      icon: 'home',
+      title: 'Home',
+    },
+  ];
+
   public readonly sidebarOpened$ = this.store
     .select(AppSidebarState.getState)
     .pipe(map(state => state.sidebarOpened));
