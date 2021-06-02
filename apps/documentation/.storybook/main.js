71f8025d80e1329c9c9e1ba2f9c360da7704cb0f
Vadim <rfprod@users.noreply.github.com>
2021-06-02 15:07:24 +0300

    fix(storybook): fix storybook compilation (#260)
    - [X] revise storybook configs;
    - [X] add missing dependencies;
    
    BREAKING CHANGE: reinstall project dependencies

--- apps/documentation/.storybook/main.js
+++ apps/documentation/.storybook/main.js
@@ -1,3 +1,15 @@
 module.exports = {
-  addons: [{ name: '@storybook/addon-knobs' }],
+  core: {
+    builder: 'webpack5',
+  },
+  addons: ['@storybook/addon-docs', '@storybook/addon-controls', '@storybook/addon-actions'],
+  features: {
+    postcss: true,
+  },
+  plugins: [
+    require('postcss-flexbugs-fixes'),
+    require('autoprefixer')({
+      flexbox: 'no-2009',
+    }),
+  ],
 };
