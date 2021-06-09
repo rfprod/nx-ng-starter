77b4ba56e9044b1838cd7408dd08cdb27970862b
Vadim <rfprod@users.noreply.github.com>
2021-06-09 10:41:18 +0300

    chore(deps+tagging): bump deps, add semver tagging (#262)
    - [X] bump project dependencies;
    - [X] verify project workability;
    - [X] add semver-tag script that can be used to set incremental versions
    
    BREAKING CHANGE: reinstall project dependencies

--- tools/shell/README.md
+++ tools/shell/README.md
@@ -14,101 +14,92 @@ This folder contains utility shell scripts.
 - `install.sh` - dependencies installation script
 - `lint.sh` - apps/libs linting script
 - `module-aliases.sh` - supported module aliases script (used by other scripts to determine if module alias exists before performing action)
+- `semver-tag.sh` - commit tagging script for versioning (eventually should be used by CI only; commits should be tagged when code is merged into the main development branch - this will require scripm improvement)
 - `set-documentation-env.sh` - documentation app prebuild script, collects references to \*.md files across the repo
 - `test.sh` - apps/libs unit testing script
 - `yarn-extension.sh` - checks package integrity and cleans up workspace if there is no integrity
 
-## `build.sh`
+## Usage
 
-### print usage
+All scripts in this directory are verbose, i.e. when a script does something it reports its progress to the terminal.
+
+Some scripts that have complex internal logic or handle different scenarios (i.e. have complex control flow) report usage errors (with examples of the correct usage) to the terminal if usage error occurs.
+
+The scripts are not intended to be used directly by developers during local development.
+
+Those scripts that are used during local development flow are integrated into the workspace by means of the `package.json` scripts. See command, and read the terminal output carefully
+
+```bash
+yarn workspace:help
+```
+
+### `build.sh` print usage
 
 ```bash
 bash tools/shell/build.sh
 ```
 
-## `changelog.sh`
-
-### print usage
+### `changelog.sh` print usage
 
 ```bash
 bash tools/shell/changelog.sh
 ```
 
-## `colors.sh`
-
-### print usage
+### `colors.sh` print usage
 
 ```bash
 bash tools/shell/colors.sh ?
 ```
 
-## `document.sh`
-
-### print usage
+### `document.sh` print usage
 
 ```bash
 bash tools/shell/document.sh
 ```
 
-## `e2e.sh`
-
-### print usage
+### `e2e.sh` print usage
 
 ```bash
 bash tools/shell/e2e.sh
 ```
 
-## `firebase-deploy.sh`
-
-### print usage
+### `firebase-deploy.sh` print usage
 
 ```bash
 bash tools/shell/firebase-deploy.sh
 ```
 
-## `generate-proto.sh`
-
-### print usage
+### `generate-proto.sh` print usage
 
 ```bash
 bash tools/shell/generate-proto.sh
 ```
 
-## `git-extension.sh`
-
-### print usage
+### `git-extension.sh` print usage
 
 ```bash
 bash tools/shell/git-extension.sh ?
 ```
 
-## `install.sh`
-
-### print usage
+### `install.sh` print usage
 
 ```bash
 bash tools/shell/install.sh
 ```
 
-## `lint.sh`
-
-### print usage
+### `lint.sh` print usage
 
 ```bash
 bash tools/shell/lint.sh
 ```
 
-## `module-aliases.sh`
-
-### print usage
+### `module-aliases.sh` print usage
 
 ```bash
 bash tools/shell/module-aliases.sh ?
 ```
 
-## `test.sh`
-
-### print usage
+### `test.sh` print usage
 
 ```bash
 bash tools/shell/test.sh
---
+++ tools/shell/semver-tag.sh
@@ -0,0 +1,296 @@
+#!/bin/bash
+
+##
+# Colors.
+##
+source tools/shell/colors.sh ''
+
+##
+# Printing utilities.
+##
+source tools/shell/print-utils.sh ''
+
+##
+# Project aliases.
+##
+source tools/shell/module-aliases.sh ''
+
+##
+# Reports usage error and exits.
+##
+reportUsageErrorAndExit() {
+  printInfoTitle "<< ${0} USAGE >>"
+  printInfoMessage "version increment exits with error if latest commit is already tagged or if checked out branch is not dev"
+  printUsageTip "bash tools/shell/version-increment.sh" "tag latest comment with an appropriate version"
+  printInfoMessage "Working versions ruleset (based on https://semver.org specification):"
+  printNameAndValue "MAJOR" "changes in one of the applications or a core application library"
+  printNameAndValue "MINOR" "changes in one of the feature libraries"
+  printNameAndValue "PATCH" "changes in one of the general purpose libraries or other files which do not belong to apps or libs folders"
+  printGap
+
+  exit 1
+}
+
+##
+# Tag latest merge commit.
+##
+setTag() {
+  printInfoTitle "<< Setting tag >>"
+  printNameAndValue "tag" "$1"
+  printGap
+
+  ##
+  # Set tag.
+  # Optionally comment when debugging script.
+  ##
+  git tag "$1"
+
+  printSuccessTitle "SUCCESS"
+  printGap
+}
+
+##
+# Generate next tag based on semver, and set tag.
+##
+semverTag() {
+  printInfoTitle "Tagging commit using semver"
+  printGap
+
+  local CHANGED_FILES
+  # Get changed files
+  # CHANGED_FILES=$(git log --pretty=format:"" --no-merges --name-only -n 1) # get changed files in the latest commit in the given branch
+  CHANGED_FILES=$(git log "$(git describe --tags --abbrev=0)"..HEAD --oneline --pretty=format:"" --no-merges --name-only) # get changed files since latest tag
+  ##
+  # Dummy changed files with with real files list for testing
+  ##
+  #CHANGED_FILES="apps/client/src/app/index.html\nlibs/client-core/src/app/index.html\nlibs/something-else/src/lib/index.html" # major changes mock
+  #CHANGED_FILES="libs/client-chatbot/src/app/index.html\nlibs/backend-grpc/src/app/index.html\nlibs/client-store/src/lib/index.html" # minor changes mock
+  #CHANGED_FILES="package.json" # patch changes mock
+
+  printNameAndValue "changed files" "${CHANGED_FILES}"
+  printGap
+
+  if [ -z "$CHANGED_FILES" ]; then
+    printErrorTitle "ERROR: no changed files found in latest commit $COMMIT_HASH in branch"
+    printGap
+    exit 1
+  else
+    local ALL_TAGS
+    local ALL_SEMVER_TAGS
+    local SEMVERS_ARRAY_INPUT
+    # Get git tags
+    ALL_TAGS=$(git tag --sort=creatordate 2>&1) # use command 'git tag -l <pattern>' later when repo has tags so that it can be tested debugged
+
+    ##
+    # Dummy tags list for testing
+    ##
+    #ALL_TAGS=$(printf "0.0.1\n0.1.0\n1.0.0\n10.1.0\n10.1.1\n10.2.0\n11.0.0\nRELEASE-1111.11.11\n")
+
+    ALL_SEMVER_TAGS=$(printf "%s" "$ALL_TAGS" | grep -o "^[0-9]*\.[0-9]*\.[0-9]*")
+
+    ##
+    # If no semver tags were found, initialize ALL_SEMVER_TAGS with a value 0.0.0
+    ##
+    if [ -z "$ALL_SEMVER_TAGS" ]; then
+      ALL_SEMVER_TAGS="0.0.0"
+    fi
+
+    printNameAndValue "ALL_SEMVER_TAGS" "$ALL_SEMVER_TAGS"
+    printGap
+
+    SEMVERS_ARRAY_INPUT=$(echo $ALL_SEMVER_TAGS | tr " " "\n")
+    while IFS=' ' read -r line; do SEMVERS_ARRAY+=("$line"); done <<<"$SEMVERS_ARRAY_INPUT"
+
+    local LATEST_SEMVER
+    local LATEST_SEMVER_MAJOR
+    local LATEST_SEMVER_MINOR
+    local LATEST_SEMVER_PATCH
+
+    LATEST_SEMVER=0.0.0
+    LATEST_SEMVER_MAJOR=$(printf "%s" "$LATEST_SEMVER" | grep -o "^[0-9]*")                # get initial latest semver major
+    LATEST_SEMVER_MINOR=$(printf "%s" "$LATEST_SEMVER" | grep -o "\.[0-9]*\." | tr -d '.') # get initial latest semver minor and remove dots
+    LATEST_SEMVER_PATCH=$(printf "%s" "$LATEST_SEMVER" | grep -o "\.[0-9]*$" | tr -d '.')  # get initial latest semver patch and remove dots
+
+    printInfoMessage "SEMVERS_ARRAY"
+    printGap
+
+    ##
+    # Get latest semver parts from SEMVERS_ARRAY
+    ##
+    for i in "${SEMVERS_ARRAY[@]}"; do
+      printNameAndValue "ARRAY_ITEM_SEMVER" "$i"
+
+      local ARRAY_ITEM_SEMVER_MAJOR
+      local ARRAY_ITEM_SEMVER_MINOR
+      local ARRAY_ITEM_SEMVER_PATCH
+
+      ARRAY_ITEM_SEMVER_MAJOR=$(printf "%s" "$i" | grep -o "^[0-9]*")                # get array item semver major
+      ARRAY_ITEM_SEMVER_MINOR=$(printf "%s" "$i" | grep -o "\.[0-9]*\." | tr -d '.') # get array item semver minor and remove dots
+      ARRAY_ITEM_SEMVER_PATCH=$(printf "%s" "$i" | grep -o "\.[0-9]*$" | tr -d '.')  # get array item semver patch and remove dots
+
+      printNameAndValue "LATEST_SEMVER_MAJOR" "$LATEST_SEMVER_MAJOR"
+      printNameAndValue "LATEST_SEMVER_MINOR" "$LATEST_SEMVER_MINOR"
+      printNameAndValue "LATEST_SEMVER_PATCH" "$LATEST_SEMVER_PATCH"
+      printNameAndValue "ARRAY_ITEM_SEMVER_MAJOR" "$ARRAY_ITEM_SEMVER_MAJOR"
+      printNameAndValue "ARRAY_ITEM_SEMVER_MINOR" "$ARRAY_ITEM_SEMVER_MINOR"
+      printNameAndValue "ARRAY_ITEM_SEMVER_PATCH" "$ARRAY_ITEM_SEMVER_PATCH"
+
+      if [ $((LATEST_SEMVER_PATCH + 0)) -lt $((ARRAY_ITEM_SEMVER_PATCH + 0)) ]; then
+        LATEST_SEMVER_MAJOR=$ARRAY_ITEM_SEMVER_MAJOR
+        LATEST_SEMVER_MINOR=$ARRAY_ITEM_SEMVER_MINOR
+        LATEST_SEMVER_PATCH=$ARRAY_ITEM_SEMVER_PATCH
+      fi
+
+      if [ $((LATEST_SEMVER_MINOR + 0)) -lt $((ARRAY_ITEM_SEMVER_MINOR + 0)) ]; then
+        LATEST_SEMVER_MAJOR=$ARRAY_ITEM_SEMVER_MAJOR
+        LATEST_SEMVER_MINOR=$ARRAY_ITEM_SEMVER_MINOR
+        LATEST_SEMVER_PATCH=0
+      fi
+
+      if [ $((LATEST_SEMVER_MAJOR + 0)) -lt $((ARRAY_ITEM_SEMVER_MAJOR + 0)) ]; then
+        LATEST_SEMVER_MAJOR=$ARRAY_ITEM_SEMVER_MAJOR
+        LATEST_SEMVER_MINOR=0
+        LATEST_SEMVER_PATCH=0
+      fi
+    done
+
+    printNameAndValue "LATEST_SEMVER_MAJOR" "$LATEST_SEMVER_MAJOR"
+    printNameAndValue "LATEST_SEMVER_MINOR" "$LATEST_SEMVER_MINOR"
+    printNameAndValue "LATEST_SEMVER_PATCH" "$LATEST_SEMVER_PATCH"
+    printGap
+
+    ##
+    # Initialize new semver parts with latest found.
+    ##
+    local MAJOR
+    MAJOR=$LATEST_SEMVER_MAJOR
+    local MINOR
+    MINOR=$LATEST_SEMVER_MINOR
+    local PATCH
+    PATCH=$LATEST_SEMVER_PATCH
+
+    ##
+    # Check which files have changed to apply increment to latest found semver.
+    # - MAJOR: matches frontend and backend apps, and core libs
+    # - MINOR: excludes previous MAJOR matches, and matches all other apps and libs
+    # - PATCH: excludes MAJOR and MINOR matches, and matches everything else
+    ##
+    local CHANGED_FILES_CONTAIN_MAJOR_CHANGES
+    CHANGED_FILES_CONTAIN_MAJOR_CHANGES=$(printf "%s" "$CHANGED_FILES" | grep -o "angular\.json\|nx\.json\|apps\/api\/.*\|apps\/client\/.*\|apps\/elements\/.*\|apps\/elements-testing\/.*\|apps\/documentation\/.*\|libs\/backend-core\/.*\|libs\/client-core\/.*")
+    local CHANGED_FILES_CONTAIN_MINOR_CHANGES
+    CHANGED_FILES_CONTAIN_MINOR_CHANGES=$(printf "%s" "$CHANGED_FILES" | grep -v "angular\.json\|nx\.json\|apps\/api\/.*\|apps\/client\/.*\|apps\/elements\/.*\|apps\/elements-testing\/.*\|apps\/documentation\/.*\|libs\/backend-core\/.*\|libs\/client-core\/.*" | grep -o "apps\/.*\|libs\/.*")
+    local CHANGED_FILES_CONTAIN_PATCH_CHANGES
+    CHANGED_FILES_CONTAIN_PATCH_CHANGES=$(printf "%s" "$CHANGED_FILES" | grep -v "angular\.json\|nx\.json\|apps\/api\/.*\|apps\/client\/.*\|apps\/elements\/.*\|apps\/elements-testing\/.*\|apps\/documentation\/.*\|libs\/backend-core\/.*\|libs\/client-core\/.*\|apps\/.*\|libs\/.*" | grep -o ".*")
+
+    printNameAndValue "CHANGED_FILES_CONTAIN_MAJOR_CHANGES" "${CHANGED_FILES_CONTAIN_MAJOR_CHANGES}"
+    printNameAndValue "CHANGED_FILES_CONTAIN_MINOR_CHANGES" "${CHANGED_FILES_CONTAIN_MINOR_CHANGES}"
+    printNameAndValue "CHANGED_FILES_CONTAIN_PATCH_CHANGES" "${CHANGED_FILES_CONTAIN_PATCH_CHANGES}"
+    printGap
+
+    ##
+    # Increment new semver parts depending on changed files.
+    ##
+    if [ -n "$CHANGED_FILES_CONTAIN_MAJOR_CHANGES" ]; then
+      MAJOR=$((MAJOR + 1))
+      MINOR=0
+      PATCH=0
+    elif [ -n "$CHANGED_FILES_CONTAIN_MINOR_CHANGES" ]; then
+      MINOR=$((MINOR + 1))
+      PATCH=0
+    elif [ -n "$CHANGED_FILES_CONTAIN_PATCH_CHANGES" ]; then
+      PATCH=$((PATCH + 1))
+    else
+      reportUsageErrorAndExit "semverTag"
+    fi
+
+    local TAG
+    TAG="${MAJOR}.${MINOR}.${PATCH}"
+
+    setTag $TAG
+
+    printf "\n\n"
+  fi
+}
+
+##
+# Check commit tag and proceed.
+##
+checkCommitTagAndProceed() {
+  printInfoTitle "<< Checking latest commit tag >>"
+  printNameAndValue "latest commit hash" "${1}"
+  printGap
+
+  local COMMIT_TAG
+  COMMIT_TAG=$(git tag --points-at "$1") # get tag which points at the latest commit hash
+
+  ##
+  # Exit with error if latest commit in given branch is already tagged
+  ##
+  if [ -n "$COMMIT_TAG" ]; then
+    reportUsageErrorAndExit "checkCommitTagAndProceed"
+  fi
+
+  semverTag
+}
+
+##
+# Sets versioning format and proceeds.
+##
+setVersioningFormatAndProceed() {
+  printInfoTitle "<< Setting versioning format and proceeding >>"
+  printNameAndValue "checked out branch" "${1}"
+  printGap
+
+  local COMMIT_HASH
+  COMMIT_HASH=$(git log --pretty=format:"%h" -n 1) # get latest commit hash in checked out branch
+
+  printNameAndValue "latest commit hash" "$COMMIT_HASH"
+  printGap
+
+  if [ -z "$COMMIT_HASH" ]; then
+    printErrorTitle "ERROR: commit not found in branch ${1}"
+    printGap
+
+    exit 1
+  fi
+
+  ##
+  # Check which branch is checked out to choose versioning format
+  ##
+  if [ "$1" = "dev" ]; then
+    checkCommitTagAndProceed "$COMMIT_HASH"
+  else
+    reportUsageErrorAndExit "setVersioningFormatAndProceed"
+  fi
+}
+
+##
+# Tag commit with version in appropriate format.
+##
+versionTagCommit() {
+  printInfoTitle "<< Tagging commit with version >>"
+  printGap
+
+  local CHECKED_OUT_BRANCH_NAME
+  CHECKED_OUT_BRANCH_NAME=$(git branch | grep -o "\*.*")
+
+  local BRANCH_NAME
+  BRANCH_NAME="${CHECKED_OUT_BRANCH_NAME//* /}" # remove pattern '* ' to get branch name only
+
+  printNameAndValue "checked out branch" "$BRANCH_NAME"
+  printGap
+
+  if [ "${BRANCH_NAME}" = "dev" ]; then
+    setVersioningFormatAndProceed "$BRANCH_NAME"
+  else
+    reportUsageErrorAndExit "versionTagCommit"
+  fi
+}
+
+##
+# Version increment control flow.
+#
+# Commits are tagged with version in semver format only for the following branches:
+# - dev
+##
+versionTagCommit
