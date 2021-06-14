3859a363839333faf56040ebad9f09c579bea5a4
rfprod <zraw.stdio@gmail.com>
2021-06-14 12:05:40 +0300

    test(unit): add unit tests
    - [X] add grpc controller unit tests

---
+++ libs/backend-grpc/src/lib/controller/grpc.controller.spec.ts
@@ -0,0 +1,65 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { nxngstarter } from '@nx-ng-starter/proto';
+import { first, tap } from 'rxjs/operators';
+
+import { NXNGSTARTER_PACKAGE } from '../grpc-client.options';
+import { BackendGrpcController } from './grpc.controller';
+
+describe('BackendGrpcController', () => {
+  let app: TestingModule;
+  const clientPackageMockData = {
+    findOne: nxngstarter.Entity.toObject(new nxngstarter.Entity(), {
+      defaults: true,
+    }),
+    findMany: [],
+  };
+
+  beforeAll(async () => {
+    app = await Test.createTestingModule({
+      controllers: [BackendGrpcController],
+      providers: [
+        {
+          provide: NXNGSTARTER_PACKAGE,
+          useValue: {
+            findOne: () => ({ ...clientPackageMockData.findOne }),
+            finaMany: () => [],
+          },
+        },
+      ],
+    }).compile();
+  });
+
+  it('getOne should return one item', async () => {
+    const appController = app.get<BackendGrpcController>(BackendGrpcController);
+    const promise = new Promise<void>((resolve, reject) => {
+      void appController
+        .getById('id1')
+        .pipe(
+          first(),
+          tap(result => {
+            expect(result).toEqual(clientPackageMockData.findOne);
+            resolve();
+          }),
+        )
+        .subscribe();
+    });
+    await promise;
+  });
+
+  it('getMany should return items length = 0', async () => {
+    const appController = app.get<BackendGrpcController>(BackendGrpcController);
+    const promise = new Promise<void>((resolve, reject) => {
+      void appController
+        .getMany()
+        .pipe(
+          first(),
+          tap(result => {
+            expect(result.length).toEqual(clientPackageMockData.findMany.length);
+            resolve();
+          }),
+        )
+        .subscribe();
+    });
+    await promise;
+  });
+});
