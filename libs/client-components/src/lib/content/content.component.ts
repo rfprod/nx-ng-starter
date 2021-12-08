d8d56335d96ccac07037817b1226ed4fb78955e9
rfprod <zoidenmacht@zoho.com>
2021-12-09 02:20:41 +0300

    fix(client): fix the content component
    - [x] scroll sidebar content top when router navigation change ends;

--- libs/client-components/src/lib/content/content.component.ts
+++ libs/client-components/src/lib/content/content.component.ts
@@ -1,8 +1,12 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
+import { MatSidenavContent } from '@angular/material/sidenav';
+import { NavigationEnd, Router } from '@angular/router';
 import { AppSidebarState, sidebarActions } from '@app/client-store-sidebar';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { Store } from '@ngxs/store';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
+@UntilDestroy()
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
@@ -10,9 +14,22 @@ import { map } from 'rxjs/operators';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppContentComponent {
+  @ViewChild('content') public readonly content?: MatSidenavContent;
+
   public readonly sidebarOpened$ = this.store.select(AppSidebarState.getState).pipe(map(state => state.sidebarOpened));
 
-  constructor(private readonly store: Store) {}
+  public readonly routerEvents$ = this.router.events.pipe(
+    tap(event => {
+      if (event instanceof NavigationEnd && typeof this.content !== 'undefined') {
+        this.content.scrollTo({ top: 0 });
+      }
+    }),
+    untilDestroyed(this),
+  );
+
+  constructor(private readonly store: Store, private readonly router: Router) {
+    void this.routerEvents$.subscribe();
+  }
 
   public sidebarCloseHandler(): void {
     void this.store.dispatch(new sidebarActions.closeSidebar());
