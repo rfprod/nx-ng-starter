d4982b7ef3ff5c9588ef5b26924807fe8c2bce56
rfprod <zoidenmacht@zoho.com>
2021-12-17 17:53:20 +0300

    build(deps): bump project dependencies
    - [x] update project dependencies to the latest versions;
    - [x] add theme toggle component stories;

---
+++ libs/client-core-components/src/lib/theme-toggle/theme-toggle.component.stories.ts
@@ -0,0 +1,63 @@
+import { APP_BASE_HREF, DOCUMENT, LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppClientMaterialModule } from '@app/client-material';
+import { AppThemeState } from '@app/client-store-theme';
+import { documentFactory, WEB_CLIENT_APP_ENV, WINDOW, windowFactory } from '@app/client-util';
+import { NgxsModule } from '@ngxs/store';
+import { Args, Story } from '@storybook/angular/types-6-0';
+
+import { AppThemeToggleComponent } from './theme-toggle.component';
+
+const testingEnvironment = {
+  production: false,
+  platform: '',
+  appName: 'Nx Ng Starter Client',
+  api: 'http://localhost:8080/api',
+  envoyUrl: 'http://localhost:8081',
+};
+
+export default {
+  title: 'AppThemeToggleComponent',
+  component: AppThemeToggleComponent,
+};
+
+const story: Story<AppThemeToggleComponent> = (args: Args) => ({
+  moduleMetadata: {
+    imports: [
+      BrowserAnimationsModule,
+      FlexLayoutModule,
+      RouterTestingModule,
+      NgxsModule.forRoot([AppThemeState]),
+      AppClientMaterialModule.forRoot(),
+    ],
+    providers: [
+      {
+        provide: LocationStrategy,
+        useClass: PathLocationStrategy,
+      },
+      { provide: WINDOW, useFactory: windowFactory },
+      { provide: DOCUMENT, useFactory: documentFactory },
+      { provide: APP_BASE_HREF, useValue: '/' },
+      {
+        provide: WEB_CLIENT_APP_ENV,
+        useValue: testingEnvironment,
+      },
+    ],
+    declarations: [AppThemeToggleComponent],
+  },
+  props: {
+    ...args,
+  },
+});
+
+export const primary = story.bind({});
+primary.args = {};
+primary.parameters = {
+  /**
+   * Use legacy Angular renderer.
+   * See docs https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#new-angular-renderer
+   */
+  angularLegacyRendering: true,
+};
