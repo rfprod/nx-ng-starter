f05d31987432863bafce2c9da9c767d5e671f5c9
rfprod <zoidenmacht@zoho.com>
2022-01-04 11:57:12 +0300

    docs(storybook): deploy storybook as part of the project docs
    - [x] improve docs - build/deploy storybook as part of the project documentation;
    - [x] clean up ci/cd pipelines;

--- tools/shell/build-docs.sh
+++ tools/shell/build-docs.sh
@@ -14,15 +14,23 @@ buildDocumentation() {
   printGap
 
   npx nx run-many --target=test --all --code-coverage --run-in-band || exit 1
-  ng run tools:coverage-stats || exit 1
+  npx nx run tools:coverage-stats || exit 1
   yarn generate:env:documentation || exit 1
 
   npx nx build --project documentation --configuration production || exit 1
 
   yarn test:reports || exit 1
-  ng run tools:compodoc-build || exit 1
+  npx nx run tools:compodoc-build || exit 1
   yarn generate:changelog || exit 1
   yarn e2e:headless:report || exit 1
+
+  ##
+  # Note.
+  # Storybook build may break from time to time due package changes.
+  # It should not block CI/CD.
+  ##
+  npx nx run documentation:build-storybook || true
+  cp -r ./dist/storybook/documentation ./dist/apps/documentation/assets/storybook || true
 }
 
 buildDocumentation
