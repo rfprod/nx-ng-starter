c1b8f55fa44c0135cbca43cae0009d5ae9df9f99
Vadim <zoidenmacht@zoho.com>
2022-01-09 10:24:22 +0300

    build(deps): bump project dependencies (#351)
    - [x] update project dependencies to the latest versions;
    - [x] regenerate proto definitions, fix generate:proto package script;

--- libs/proto/src/lib/grpc/root_pb.js
+++ libs/proto/src/lib/grpc/root_pb.js
@@ -13,7 +13,13 @@
 
 var jspb = require('google-protobuf');
 var goog = jspb;
-var global = Function('return this')();
+var global = (function() {
+  if (this) { return this; }
+  if (typeof window !== 'undefined') { return window; }
+  if (typeof global !== 'undefined') { return global; }
+  if (typeof self !== 'undefined') { return self; }
+  return Function('return this')();
+}.call(null));
 
 var common_pb = require('./common_pb.js');
 goog.object.extend(proto, common_pb);
