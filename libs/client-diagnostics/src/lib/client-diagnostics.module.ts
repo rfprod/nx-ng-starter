5d313700d76fbffe9419eda13168a2faf1a0cf8f
Vadim <zoidenmacht@zoho.com>
2022-01-10 19:04:27 +0300

    build(deps): bump storybook to 6.5.0-alpha.7, refactor libraries (#352)
    - [x] update storybook version to 6.5.0-alpha.7;
    - [x] refactor libraries, put components into the components directory;

--- libs/client-diagnostics/src/lib/client-diagnostics.module.ts
+++ libs/client-diagnostics/src/lib/client-diagnostics.module.ts
@@ -4,11 +4,11 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppClientMaterialModule } from '@app/client-material';
 
 import { AppClientDiagnosticsRoutingModule } from './client-diagnostics-routing.module';
-import { AppDiagnosticsHomeComponent } from './home/diagnostics-home.component';
-import { AppDiagnosticsHomePage } from './home/page/diagnostics-home-page.component';
-import { AppDiagnosticsIndexComponent } from './index/diagnostics-index.component';
-import { AppDiagnosticsInfoComponent } from './info/diagnostics-info.component';
-import { AppDiagnosticsInfoPage } from './info/page/diagnostics-info-page.component';
+import { AppDiagnosticsHomeComponent } from './components/home/diagnostics-home.component';
+import { AppDiagnosticsHomePage } from './components/home/page/diagnostics-home-page.component';
+import { AppDiagnosticsIndexComponent } from './components/index/diagnostics-index.component';
+import { AppDiagnosticsInfoComponent } from './components/info/diagnostics-info.component';
+import { AppDiagnosticsInfoPage } from './components/info/page/diagnostics-info-page.component';
 
 @NgModule({
   imports: [FlexLayoutModule, CommonModule, AppClientMaterialModule, AppClientDiagnosticsRoutingModule],
