f85220ee8efeed11e778154fe182c0f147925df3
Vadim <zoidenmacht@zoho.com>
2022-01-27 22:45:09 +0300

    test(unit): update jest coverage collector config, bump thresholds (#369)
    - [x] update jest preset - coverage collector ignored files;
    - [x] bump unit test coverage thresholds;

--- apps/documentation/jest.config.js
+++ apps/documentation/jest.config.js
@@ -10,9 +10,9 @@ module.exports = {
     // TODO: bump unit test coverage and remove this override
     global: {
       branches: 0,
-      functions: 0,
-      lines: 0,
-      statements: 0,
+      functions: 42,
+      lines: 87,
+      statements: 88,
     },
   },
 };
