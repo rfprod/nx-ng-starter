3f836716ba5ca711b925fb65be2e5cd222a95fa7
rfprod <zoidenmacht@zoho.com>
2022-01-28 16:28:23 +0300

    build(deps): bump project dependencies, increase unit test coverage
    - [x] update project dependencies to the latest versions;
    - [x] increase unit test coverage,
    regenerate unit coverage stats;

---
+++ apps/elements/src/app/services/elements.service.spec.ts
@@ -0,0 +1,38 @@
+import { Injector } from '@angular/core';
+import { TestBed, TestModuleMetadata, waitForAsync } from '@angular/core/testing';
+import { getTestBedConfig, newTestBedMetadata } from '@app/client-unit-testing';
+import { WINDOW, windowProvider } from '@app/client-util';
+
+import { AppElementsService } from './elements.service';
+
+describe('AppElementsService', () => {
+  const testBedMetadata: TestModuleMetadata = newTestBedMetadata({
+    providers: [Injector, windowProvider],
+  });
+  const testBedConfig: TestModuleMetadata = getTestBedConfig(testBedMetadata);
+
+  let service: AppElementsService;
+
+  let win: Window;
+
+  beforeEach(
+    waitForAsync(() => {
+      void TestBed.configureTestingModule(testBedConfig)
+        .compileComponents()
+        .then(() => {
+          service = TestBed.inject(AppElementsService);
+          win = TestBed.inject(WINDOW);
+        });
+    }),
+  );
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('registerElements should register all custom web elements', () => {
+    service.registerElements();
+    const chatbotWidget = win.customElements.get('app-chatbot-root');
+    expect(chatbotWidget).not.toBeUndefined();
+  });
+});
