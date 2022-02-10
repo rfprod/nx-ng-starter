7694b4ca70d2218f18ee9fb8d5b80d54c58fe967
rfprod <zoidenmacht@zoho.com>
2022-02-10 20:38:08 +0300

    test(unit): add client sidebar component spec
    - [x] implement client sidebar root component spec;

--- libs/client-sidebar/src/lib/components/sidebar-root/sidebar-root.component.spec.ts
+++ libs/client-sidebar/src/lib/components/sidebar-root/sidebar-root.component.spec.ts
@@ -1,1 +1,59 @@
-test.todo('AppSidebarRootComponent');
+import { ComponentFixture, TestBed, TestModuleMetadata, waitForAsync } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppHttpProgressState } from '@app/client-store-http-progress';
+import { sidebarActions } from '@app/client-store-sidebar';
+import { AppTestingComponent, getTestBedConfig, newTestBedMetadata } from '@app/client-unit-testing';
+import { Navigate } from '@ngxs/router-plugin';
+import { NgxsModule, Store } from '@ngxs/store';
+
+import { AppSidebarRootComponent } from './sidebar-root.component';
+
+describe('AppSidebarRootComponent', () => {
+  const testBedMetadata: TestModuleMetadata = newTestBedMetadata({
+    declarations: [AppSidebarRootComponent],
+    imports: [
+      NgxsModule.forFeature([AppHttpProgressState]),
+      RouterTestingModule.withRoutes([
+        {
+          path: 'info',
+          component: AppTestingComponent,
+        },
+      ]),
+    ],
+  });
+  const testBedConfig: TestModuleMetadata = getTestBedConfig(testBedMetadata);
+
+  let fixture: ComponentFixture<AppSidebarRootComponent>;
+  let component: AppSidebarRootComponent;
+
+  let store: Store;
+  let storeDispatchSpy: jest.SpyInstance;
+
+  beforeEach(
+    waitForAsync(() => {
+      void TestBed.configureTestingModule(testBedConfig)
+        .compileComponents()
+        .then(() => {
+          fixture = TestBed.createComponent(AppSidebarRootComponent);
+          component = fixture.componentInstance;
+          store = TestBed.inject(Store);
+          storeDispatchSpy = jest.spyOn(store, 'dispatch');
+          fixture.detectChanges();
+        });
+    }),
+  );
+
+  it('should be defined', () => {
+    expect(component).toBeDefined();
+  });
+
+  it('sidebarCloseHandler should call store dispatch', () => {
+    component.sidebarCloseHandler();
+    expect(storeDispatchSpy).toHaveBeenCalledWith(new sidebarActions.closeSidebar());
+  });
+
+  it('navigateToInfoPage should call store dispatch', () => {
+    component.navigateToInfoPage();
+    expect(storeDispatchSpy).toHaveBeenCalledWith(new Navigate([{ outlets: { primary: 'info' } }]));
+  });
+});
