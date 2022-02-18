fb729eb6ba38d359cf72ca51f76a25e37ccf1324
rfprod <zoidenmacht@zoho.com>
2022-02-18 10:53:37 +0300

    fix(docs): docs - user dom sanitizer to sanitize file readme file path
    - [x] use dom sanitizer before feeding markdown file to marked component src;

--- apps/documentation/src/app/componenets/md-reference/md-reference.component.ts
+++ apps/documentation/src/app/componenets/md-reference/md-reference.component.ts
@@ -1,5 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, SecurityContext } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Store } from '@ngxs/store';
+import { map } from 'rxjs';
 
 import { AppMdFilesState } from '../../modules/store/md-files/md-files.state';
 
@@ -13,7 +15,9 @@ export class AppDocMarkdownReferenceComponent {
   /**
    * Selected markdown file path.
    */
-  public readonly filePath$ = this.store.select(AppMdFilesState.filePath);
+  public readonly filePath$ = this.store
+    .select(AppMdFilesState.filePath)
+    .pipe(map(filePath => this.sanitizer.sanitize(SecurityContext.HTML, filePath)));
 
-  constructor(private readonly store: Store) {}
+  constructor(private readonly store: Store, private readonly sanitizer: DomSanitizer) {}
 }
