cfa7a61b6a17ef2d43adf8f19916bc1564f2278d
Vadim <zoidenmacht@zoho.com>
2022-03-13 20:33:52 +0300

    fix(charts): unit test and chart improvements (#415)
    - [x] add force directed chart to examples;
    - [x] increase sidebar module unit test coverage;
    - [x] fix client root layout- only the content block should be scrollable;

--- libs/client-d3-charts/src/lib/interfaces/pie-chart.interface.ts
+++ libs/client-d3-charts/src/lib/interfaces/pie-chart.interface.ts
@@ -4,9 +4,7 @@ export interface IPieChartDataNode {
 }
 
 export enum PIE_CHART_ARC_CONFIG {
-  ARC_OUTER_RADIUS = 10,
   ARC_INNER_RADIUS = 50,
-  LABEL_OUTER_RADIUS = 40,
   LABEL_INNER_RADIUS = 40,
 }
 
