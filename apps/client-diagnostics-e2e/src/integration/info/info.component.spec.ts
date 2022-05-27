d735560895e7edf7034fb875ed47082205d75311
Vadim <zoidenmacht@zoho.com>
2022-05-28 01:20:22 +0300

    test(e2e): revise and fix e2e tests (#481)
    - [x] fix basic e2e tests so that it passes;

--- apps/client-diagnostics-e2e/src/integration/info/info.component.spec.ts
+++ apps/client-diagnostics-e2e/src/integration/info/info.component.spec.ts
@@ -1,7 +1,13 @@
+import { getIframe } from '../../support/commands';
+
 describe('info component', () => {
   beforeEach(() => cy.visit('/?path=/story/appdiagnosticsinfopage--primary'));
 
   it('should render the component', () => {
-    cy.get('app-diagnostics-info-page').should('exist');
+    getIframe().find('div[id="root"]').should('not.be.empty');
+
+    getIframe().find('storybook-dynamic-app-root').should('not.be.empty');
+
+    getIframe().find('app-diagnostics-info-page').should('not.be.empty');
   });
 });
--- apps/client-diagnostics-e2e/src/support/commands.ts
+++ apps/client-diagnostics-e2e/src/support/commands.ts
@@ -23,3 +23,6 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+export const getIframe = () =>
+  cy.get('iframe[id="storybook-preview-iframe"]').its('0.contentDocument.body').should('not.be.empty').then(cy.wrap);
