e5ad94ac0bf6e12319f7387058d936fc4577fdfa
rfprod <zoidenmacht@zoho.com>
2022-06-09 02:27:54 +0300

    build(deps): bump project dependencies
    - [x] update project dependencies to the latest versions;

--- tools/shell/install.sh
+++ tools/shell/install.sh
@@ -43,7 +43,7 @@ installGlobalDependencies() {
   printInfoTitle "<< INSTALLING GLOBAL DEPENDENCIES >>"
   printGap
 
-  sudo npm install -g @angular/cli@latest @nestjs/cli@latest @ngxs/cli@latest @nrwl/cli@latest typescript@latest @compodoc/compodoc@latest commitizen@latest cz-conventional-changelog@latest clang-format@latest yarn@1.22.18 madge@latest npm-check-updates@latest || exit 1
+  sudo npm install -g @angular/cli@latest @nestjs/cli@latest @ngxs/cli@latest @nrwl/cli@latest typescript@latest @compodoc/compodoc@latest commitizen@latest cz-conventional-changelog@latest clang-format@latest yarn@1.22.19 madge@latest npm-check-updates@latest || exit 1
 }
 
 ##
