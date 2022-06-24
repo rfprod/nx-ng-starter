f4f4e5ed00da4cb0256f81bf8f1c1f0bad9256b5
rfprod <zoidenmacht@zoho.com>
2022-06-25 00:06:49 +0300

    fix(sec): fix trusted types default policy create script

--- tools/js/trusted-types.policy.js
+++ tools/js/trusted-types.policy.js
@@ -1,13 +1,19 @@
 // See more here: https://web.dev/trusted-types/#create-a-default-policy
 
 /**
+ * This global variable is declared by declaring "node_modules/dompurify/dist/purify.min.js"
+ * in angular.json `scripts` array.
+ */
+var DOMPurify;
+
+/**
  * Creates a default Trusted Types policy.
  * This file is added to client frontend applications via angular.json `scripts` array.
  */
 if (window.trustedTypes && window.trustedTypes.createPolicy) {
   window.trustedTypes.createPolicy('default', {
-    createHTML: string => global.DOMPUrify.sanitize(string, { RETURN_TRUSTED_TYPE: true }),
-    createScript: string => global.DOMPUrify.sanitize(string, { RETURN_TRUSTED_TYPE: true }),
-    createScriptURL: string => global.DOMPUrify.sanitize(string, { RETURN_TRUSTED_TYPE: true }),
+    createHTML: string => DOMPurify.sanitize(string, { RETURN_TRUSTED_TYPE: true }),
+    createScript: string => DOMPurify.sanitize(string, { RETURN_TRUSTED_TYPE: true }),
+    createScriptURL: string => DOMPurify.sanitize(string, { RETURN_TRUSTED_TYPE: true }),
   });
 }
