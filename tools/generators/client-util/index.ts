af46ba98420313bee367938f485bc7ac6d222b32
Vadim <zoidenmacht@zoho.com>
2022-08-05 15:17:16 +0300

    build(deps): bump project dependencies, update generators (#548)
    - [x] revise and update workspace generators, bump project dependencies;

--- tools/generators/client-util/index.ts
+++ tools/generators/client-util/index.ts
@@ -1,115 +1,76 @@
-import { strings } from '@angular-devkit/core';
+import { libraryGenerator } from '@nrwl/angular/generators';
 import {
-  apply,
-  chain,
-  externalSchematic,
-  MergeStrategy,
-  mergeWith,
-  move,
-  Rule,
-  SchematicContext,
-  template,
+  generateFiles,
+  joinPathFragments,
+  ProjectConfiguration,
+  readProjectConfiguration,
   Tree,
-  url,
-} from '@angular-devkit/schematics';
-import { createOrUpdate, deleteFile, formatFiles, getProjectConfig } from '@nrwl/workspace';
-import * as fs from 'fs';
+  updateProjectConfiguration,
+} from '@nrwl/devkit';
+import { unlink } from 'fs/promises';
 import { fileExists } from 'nx/src/utils/fileutils';
 
 import { ISchematicContext } from './schema.interface';
 
-interface IProjectConfig {
-  root: string;
-  sourceRoot: string;
-  projectType: string;
-  prefix: string;
-  architect: {
-    lint: {
-      builder: string;
-      options: {
-        eslintConfig: string;
-        lintFilePatterns: string[];
-      };
-      outputs: string[];
-    };
-    test: {
-      builder: string;
-      outputs: string[];
-      options: {
-        jestConfig: string;
-        passWithNoTests: boolean;
-      };
-    };
-  };
-}
+/**
+ * Adds/replaces files generated for a library by default.
+ */
+const addFiles = (schema: ISchematicContext, tree: Tree) => {
+  const config: ProjectConfiguration = readProjectConfiguration(tree, schema.name);
+  const root = config.root;
 
-const addFiles =
-  (schema: ISchematicContext): Rule =>
-  (tree: Tree, context: SchematicContext) => {
-    const projectConfig: IProjectConfig = getProjectConfig(tree, schema.name);
-
-    const templateSource = apply(url('./files'), [
-      move(projectConfig.root),
-      template({
-        ...strings,
-        ...schema, // pass the objects containing the properties & functions to be used in template files.
-        tmpl: '', // this value is used to files naming to that template files do not affect development processes.
-      }),
-    ]);
-
-    return chain([mergeWith(templateSource, MergeStrategy.Overwrite)])(tree, context);
-  };
+  generateFiles(tree, joinPathFragments(__dirname, './files'), root, {
+    tmpl: '',
+    name: 'myscript',
+  });
+};
 
 /**
- * Updates angular.json
+ * Updates project.json
  */
-const updateProjectConfig =
-  (schema: ISchematicContext): Rule =>
-  (tree: Tree, context: SchematicContext) => {
-    const projectConfig: IProjectConfig = getProjectConfig(tree, schema.name);
-    projectConfig.architect.lint.builder = '@angular-eslint/builder:lint';
-    projectConfig.architect.lint.options.eslintConfig = `libs/${schema.name}/.eslintrc.json`;
-    projectConfig.architect.lint.options.lintFilePatterns = [`libs/${schema.name}/**/*.ts`];
-    projectConfig.architect.lint.outputs = ['{options.outputFile}'];
+const updateProjectConfig = (schema: ISchematicContext, tree: Tree) => {
+  const projectConfig: ProjectConfiguration = readProjectConfiguration(tree, schema.name);
+  if (typeof projectConfig.targets !== 'undefined') {
+    projectConfig.targets.lint.executor = '@angular-eslint/builder:lint';
+    projectConfig.targets.lint.options = {
+      ...(projectConfig.targets.lint.options ?? {}),
+      eslintConfig: `libs/${schema.name}/.eslintrc.json`,
+      lintFilePatterns: [`libs/${schema.name}/**/*.ts`],
+    };
+    projectConfig.targets.lint.outputs = ['{options.outputFile}'];
+  }
 
-    const projectRoot = `${process.cwd()}`;
-    const angularJsonPath = `${projectRoot}/angular.json`;
-
-    const angularJson: Record<string, Record<string, unknown>> = JSON.parse(fs.readFileSync(angularJsonPath)?.toString() ?? '{}');
-    angularJson.projects[schema.name] = projectConfig;
-
-    createOrUpdate(tree, './angular.json', JSON.stringify(angularJson));
-
-    return chain([formatFiles({ skipFormat: false }, './angular.json')])(tree, context);
-  };
+  updateProjectConfiguration(tree, schema.name, projectConfig);
+};
 
 /**
  * Removes unneeded files.
  */
-const cleanup =
-  (schema: ISchematicContext): Rule =>
-  (tree: Tree, context: SchematicContext) => {
-    const root = `${process.cwd()}`;
-    const rootEslintJson = `${root}/.eslintrc.json`;
-    const eslintJsonExists = fileExists(rootEslintJson);
-    return chain(eslintJsonExists ? [deleteFile(rootEslintJson)] : [])(tree, context);
-  };
+const cleanup = async (): Promise<void> => {
+  const root = `${process.cwd()}`;
+  const rootEslintJson = `${root}/.eslintrc.json`;
+  const eslintJsonExists = fileExists(rootEslintJson);
+  if (eslintJsonExists) {
+    await unlink(rootEslintJson);
+  }
+};
 
-export default function (schema: ISchematicContext) {
-  return (tree: Tree, context: SchematicContext) => {
-    const name = schema.name;
-    const tags = schema.tags;
+export default async function (tree: Tree, schema: ISchematicContext) {
+  const name = schema.name;
+  const tags = schema.tags;
 
-    return chain([
-      chain([
-        externalSchematic('@nrwl/angular', 'lib', {
-          name,
-          tags,
-        }),
-        addFiles(schema),
-      ]),
-      updateProjectConfig(schema),
-      cleanup(schema),
-    ])(tree, context);
-  };
+  await libraryGenerator(tree, {
+    name,
+    prefix: 'app',
+    standaloneConfig: true,
+    tags,
+  });
+
+  addFiles(schema, tree);
+
+  updateProjectConfig(schema, tree);
+
+  await cleanup();
+
+  return () => ({ success: true });
 }
