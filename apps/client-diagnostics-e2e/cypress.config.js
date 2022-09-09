f4782d1aeaf1d64361978ad0de1d25fe718e890b
rfprod <zoidenmacht@zoho.com>
2022-09-09 22:39:06 +0300

    test(e2e): migrate cypress configurations
    - [x] migrate e2e app configurations to cypress 10;

---
+++ apps/client-diagnostics-e2e/cypress.config.js
@@ -0,0 +1,26 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  experimentalStudio: true,
+  chromeWebSecurity: false,
+  fileServerFolder: '.',
+  fixturesFolder: './src/fixtures',
+  modifyObstructiveCode: false,
+  reporter: '../../node_modules/cypress-multi-reporters/index.js',
+  reporterOptions: {
+    configFile: 'cypress-reporter-options.json',
+  },
+  screenshotsFolder: '../../dist/cypress/apps/client-diagnostics-e2e/screenshots',
+  video: true,
+  videosFolder: '../../dist/cypress/apps/client-diagnostics-e2e/videos',
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./src/plugins/index.ts').default(on, config);
+    },
+    baseUrl: 'http://localhost:4400',
+    specPattern: './src/integration/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: './src/support/index.ts',
+  },
+});
