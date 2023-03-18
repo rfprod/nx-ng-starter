e7600206c77fe88b8e145630fdcd8e0075b1e029
Vadim <zoidenmacht@zoho.com>
2023-03-18 02:06:36 +0200

    build(deps): bump project dependencies (#676)
    - [x] update project dependencies to the latest versions;
    - [x] fix diagnostics info page layout;

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,7 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 module.exports = {
   core: {
     builder: 'webpack5',
+    disableTelemetry: true
   },
   addons: [
     {
--- libs/backend-interfaces/src/lib/graphql/upload/dto/file-upload-input.dto.ts
+++ libs/backend-interfaces/src/lib/graphql/upload/dto/file-upload-input.dto.ts
@@ -1,5 +1,5 @@
 import { Field, InputType } from '@nestjs/graphql';
-import { FileUpload } from 'graphql-upload/Upload';
+import { FileUpload } from 'graphql-upload/Upload.mjs';
 
 @InputType()
 export class AppFileUploadInputDto implements Partial<FileUpload> {
