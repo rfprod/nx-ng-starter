6fb0c3716bd7490ddc24e72cb44ff329d9260d48
Vadim <zoidenmacht@zoho.com>
2023-03-31 21:51:30 +0300

    build(shell): deprecate some shell script commands (#681)
    - [x] deprecate shell script commands that duplicate nx commands;

--- tools/shell/build-docs.sh
+++ tools/shell/build-docs.sh
@@ -8,7 +8,7 @@ buildDocumentation() {
   printInfoTitle "<< Building documentation app >>"
   printGap
 
-  npx nx run-many --target=test --all --code-coverage --run-in-band || exit 1
+  npx nx run-many --target test --all --code-coverage --run-in-band || exit 1
   npx nx run tools:coverage-stats || exit 1
   yarn generate:env:documentation || exit 1
 
@@ -16,14 +16,15 @@ buildDocumentation() {
   npx nx build --project documentation --configuration production || exit 1
   npx nx run documentation:configure-env --reset || exit 1
 
-  yarn test:reports || exit 1
+  bash tools/shell/test.sh reports || exit 1
   yarn generate:unit-test-coverage-index || exit 1
 
   npx nx run tools:compodoc-build || exit 1
   cp -r ./dist/compodoc ./dist/apps/documentation/assets || exit 1
 
   yarn generate:changelog || exit 1
-  yarn e2e:report || exit 1
+  npx nx run-many --target e2e --all
+  bash tools/shell/e2e.sh reports || exit 1
   yarn generate:e2e-test-report-index || exit 1
 
   npx nx run documentation:build-storybook || exit 1
