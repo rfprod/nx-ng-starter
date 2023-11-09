3ac4ce2de445c1d797adb96a438fa05bf2d56839
rfprod <zoidenmacht@zoho.com>
2023-11-09 16:23:09 +0200

    build(deps): bump project dependencies
    - [x] udpate npm dependencies;

--- libs/client-util-sentry/src/test-setup.ts
+++ libs/client-util-sentry/src/test-setup.ts
@@ -1,1 +1,11 @@
 import 'jest-preset-angular/setup-jest';
+
+const traceService = 'TraceService';
+const browserTracing = 'BrowserTracing';
+
+jest.mock('@sentry/angular-ivy', () => ({
+  [traceService]: jest.fn().mockReturnValue({}),
+  [browserTracing]: jest.fn().mockReturnValue({}),
+  init: jest.fn().mockReturnValue({}),
+  createErrorHandler: jest.fn().mockReturnValue({}),
+}));
